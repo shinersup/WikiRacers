ff45017da0a9d38f0c6b758657ab4684c03ad337
Treehugger255 <ethan.eschrich@gmail.com>
2024-12-04 18:49:44 -0500

    Implement Graph.h interface, which stores a pointer to the graph object, the from vertex, the to vertex, the path found, and the set of visited vertices (Issue #2)

--- src/Graph.h
+++ src/Graph.h
@@ -9,19 +9,39 @@
 #include <sstream>
 #include <iostream>
 #include <string>
-#include <map>
+#include <utility>
+#include <set>
+#include <vector>
 
 using namespace std;
 
+// Wrapper class for Graph
+// Abstracts both traversals and parsing of the inputs
 class Graph
 {
 private:
     Agraph_t* g;
+    Agnode_t* from;
+    Agnode_t* to;
+    vector<Agnode_t*> path;
+    set<Agnode_t*> visited;
+
+    bool parse_graph(string path);
+    // Wrapper function for getNode, so that we can pass in a string and not a mutable c_str
+    Agnode_t* getNode(string s);
 
 public:
-    bool parse_graph(string path);
-    Graph();
+    Graph(string path);
     ~Graph();
+
+    // Set the start and end vertices
+    // Allows for reusing the same graph object for multiple runs
+    void setFromTo(string start, string end);
+
+    // Algorithms for traversing the graph
+    // Returns a pair of a vector of the pointers to nodes in the path found, and the set of pointers
+    // to nodes visited
+    void dfs();
+    void bfs();
+    void dijkstras();
 };
-
-
